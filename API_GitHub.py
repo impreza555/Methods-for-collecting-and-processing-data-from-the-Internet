662489c772aa306c2fb8f072e9a8712bce1694b3
impreza555 <alex.erjomin@outlook.com>
2022-01-18 22:55:00 +0300

    Небольшое исправление.

--- API_GitHub.py
+++ API_GitHub.py
@@ -10,6 +10,6 @@ token = os.environ.get('GITHUB_TOKEN')
 repos = requests.get('https://api.github.com/user/repos', auth=(username, token))
 with open("api_github.json", "w") as file:
     json.dump(repos.json(), file, sort_keys=True, ensure_ascii=False, indent=4)
-    for repo in repos.json():
-        print(f'Название репозитория: {repo["name"]}')
-        print(f'URL репозитория: {repo["html_url"]}')
+for repo in repos.json():
+    print(f'Название репозитория: {repo["name"]}')
+    print(f'URL репозитория: {repo["html_url"]}')
