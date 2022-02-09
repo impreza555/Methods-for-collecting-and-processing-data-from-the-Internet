4072e45bfb6d04a71059433de5c79d5963df8122
impreza555 <alex.erjomin@outlook.com>
2022-02-09 03:19:37 +0300

    Сделал ДЗ 6.

---
+++ books/runner.py
@@ -0,0 +1,13 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+
+from books import settings
+from books.spiders.book24 import Book24Spider
+
+if __name__ == '__main__':
+    search_string = 'python'
+    crawler_settings = Settings()
+    crawler_settings.setmodule(settings)
+    process = CrawlerProcess(settings=crawler_settings)
+    process.crawl(Book24Spider, search_string)
+    process.start()
